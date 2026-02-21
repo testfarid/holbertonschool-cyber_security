82df21128cb789aa78fde67a970900f28eeb3101
testfarid <11293@holbertonstudents.com>
2026-02-21 02:43:45 -0500

    test

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F: '/Registrant|Admin|Tech/ { gsub(/^ +| +$/,"",$2); print $1","$2 }'
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/ && !/ID:/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); if($1 ~ /Ext:$/) print $1", "; else if($1 ~ /Street$/) print $1", "$2; else print $1", "$2}' > "${1}.csv"
